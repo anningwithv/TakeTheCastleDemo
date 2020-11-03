104602a8f8995cef4b65cc47fb42bd57a2bc997d
zhangwei_modooplay <wei.zhang@modooplay.com>
2020-11-03 15:33:02 +0800

    checkpoint

---
+++ TakeThatCastleDemo/Assets/Scripts/CheckPoint.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckPoint : MonoBehaviour
+{
+    public int index;
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawSphere(transform.position, 1f);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //TODO:Check triggered by our soldier
+
+        CheckPointMgr.S.OnCheckPointTriggered(this);
+    }
+}
---
+++ TakeThatCastleDemo/Assets/Scripts/CheckPointMgr.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Qarth;
+using System.Linq;
+
+public class CheckPointMgr : TMonoSingleton<CheckPointMgr>
+{
+    public List<CheckPoint> checkPointList = new List<CheckPoint>();
+
+    private CheckPoint m_CurCheckPoint = null;
+
+    private void Start()
+    {
+        m_CurCheckPoint = checkPointList[0];
+    }
+
+    public void OnCheckPointTriggered(CheckPoint checkPoint)
+    {
+        if (checkPoint.index > m_CurCheckPoint.index)
+        {
+            m_CurCheckPoint = checkPoint;
+        }
+        else
+        {
+            if (checkPoint == checkPointList.LastOrDefault())
+            {
+                Log.i("Game win");
+            }
+        }
+    }
+
+    public Vector3 GetSoldierSpawnPos()
+    {
+        return m_CurCheckPoint.transform.position;
+    }
+
+    public Vector3 GetSoldierTargetPos()
+    {
+        CheckPoint checkPoint = checkPointList.Where(i => i.index == m_CurCheckPoint.index + 1).FirstOrDefault();
+        if (checkPoint != null)
+        {
+            return checkPoint.transform.position;
+        }
+
+        return checkPointList.LastOrDefault().transform.position;
+    }
+}
