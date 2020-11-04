400767003d0ac172b14548ac8560f4e5d6e7cc6c
zhangwei_modooplay <wei.zhang@modooplay.com>
2020-11-04 15:57:15 +0800

    ui

---
+++ TakeThatCastleDemo/Assets/Scripts/UI/MainGamePanel.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainGamePanel : MonoBehaviour
+{
+    public Button m_Btn1;
+    public Button m_Btn2;
+    public Button m_Btn3;
+    public Text m_TextCoin;
+
+    private int m_CoinValue = 10000;
+
+    private void Start()
+    {
+        m_Btn1.onClick.AddListener(()=> {
+
+        });
+
+        m_Btn2.onClick.AddListener(() => {
+
+        });
+
+        m_Btn3.onClick.AddListener(() => {
+
+        });
+
+        SetCoinText();
+    }
+
+    private void SetCoinText()
+    {
+        m_TextCoin.text = m_CoinValue.ToString();
+    }
+}
